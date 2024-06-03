0cda131da148c2f4de9540aa4f8b433efb18f76a
miguel69645 <miancarrilloal@ittepic.edu.mx>
2024-06-03 14:24:06 -0700

    cambios de ayer

--- src/security/precios/components/tables/PreciosTable.jsx
+++ src/security/precios/components/tables/PreciosTable.jsx
@@ -1,120 +1,120 @@
-//FIC: React
 import React, { useEffect, useMemo, useState } from "react";
-//FIC: Material UI
-import { MaterialReactTable } from "material-react-table";
-import { Box, Stack, Tooltip, Button, IconButton, Dialog } from "@mui/material";
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+} from "material-react-table";
+import { Box, Stack, Tooltip, IconButton, Dialog } from "@mui/material";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import EditIcon from "@mui/icons-material/Edit";
 import InfoIcon from "@mui/icons-material/Info";
 import DeleteIcon from "@mui/icons-material/Delete";
-//FIC: DB
-//import PreciosStaticData from '../../../../../db/security/json/Precios/PreciosData';
 import { getAllPrecios } from "../../services/remote/get/getAllPrecios";
-//FIC: Modals
 import AddPrecioModal from "../modals/AddPrecioModal";
-import { useSelector, useDispatch } from "react-redux";
-//FIC: Columns Table Definition.
-const PreciosColumns = [
-  {
-    accessorKey: "IdProdServOK",
-    header: "ID PROD SERV OK",
-    size: 150, //small column
-  },
-  {
-    accessorKey: "IdPresentaBK",
-    header: "ID PRESENTA OK",
-    size: 200 //small column
-  },
-  {
-    accessorKey: "PresentacionDelProducto",
-    header: "DESCRIPCION",
-    size: 600, //small column
-  },
-  {
-    accessorKey: "IdTipoFormulaOK",
-    header: "ID TIPO FORMUILA OK",
-    size: 150, //small column
-  },
-  {
-    accessorKey: "Formula",
-    header: "FORMULA",
-    size: 150, //small column
-  },
-  {
-    accessorKey: "Precio",
-    header: "PRECIO",
-    size: 150, //small column
-  },
-];
-//FIC: Table - FrontEnd.
+import { useSelector } from "react-redux";
+
 const PreciosTable = () => {
   const id = useSelector((state) => state.institutes.institutesDataArr);
   console.log(id);
 
-  //FIC: controlar el estado del indicador (loading).
   const [loadingTable, setLoadingTable] = useState(true);
-
-  //FIC: controlar el estado de la data de Institutos.
   const [PreciosData, setPreciosData] = useState([]);
-  //FIC: controlar el estado que muesta u oculta la modal de nuevo Instituto.
+  const [rowSelection, setRowSelection] = useState({});
   const [AddPrecioShowModal, setAddPrecioShowModal] = useState(false);
+  const [selectedPrecioId, setSelectedPrecioId] = useState(null);
+
   useEffect(() => {
     async function fetchData() {
       try {
         const AllPreciosData = await getAllPrecios(id);
         setPreciosData(AllPreciosData);
-        //setPreciosData(PreciosStaticData);
         setLoadingTable(false);
+
+        // Set the first row as selected
+        if (AllPreciosData.length > 0) {
+          setRowSelection({ [AllPreciosData[0]._id]: true });
+        }
       } catch (error) {
         console.error(
-          "Error al obtener los institutos en useEffect de PreciosTable:",
+          "Error al obtener los precios en useEffect de PreciosTable:",
           error
         );
       }
     }
     fetchData();
-  }, []);
+  }, [id]);
+
+  const handleRowClick = (row) => {
+    setSelectedPrecioId(row.original._id);
+    setRowSelection((prev) => ({
+      [row.id]: !prev[row.id],
+    }));
+  };
+
+  const PreciosColumns = useMemo(
+    () => [
+      { accessorKey: "IdProdServOK", header: "ID PROD SERV OK", size: 150 },
+      { accessorKey: "IdPresentaBK", header: "ID PRESENTA OK", size: 200 },
+      {
+        accessorKey: "PresentacionDelProducto",
+        header: "DESCRIPCION",
+        size: 600,
+      },
+      {
+        accessorKey: "IdTipoFormulaOK",
+        header: "ID TIPO FORMULA OK",
+        size: 150,
+      },
+      { accessorKey: "Formula", header: "FORMULA", size: 150 },
+      { accessorKey: "Precio", header: "PRECIO", size: 150 },
+    ],
+    []
+  );
+
+  const table = useMaterialReactTable({
+    columns: PreciosColumns,
+    data: PreciosData,
+    getRowId: (row) => row._id,
+    muiTableBodyRowProps: ({ row }) => ({
+      onClick: () => handleRowClick(row),
+      selected: !!rowSelection[row.id],
+      sx: {
+        cursor: "pointer",
+        backgroundColor: rowSelection[row.id] ? "lightgreen" : "white",
+      },
+    }),
+    onRowSelectionChange: setRowSelection,
+    state: { rowSelection, isLoading: loadingTable },
+    renderBottomToolbarCustomActions: () => (
+      <Stack direction="row" sx={{ m: 1 }}>
+        <Box>
+          <Tooltip title="Agregar">
+            <IconButton onClick={() => setAddPrecioShowModal(true)}>
+              <AddCircleIcon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Editar">
+            <IconButton>
+              <EditIcon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Eliminar">
+            <IconButton>
+              <DeleteIcon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Detalles">
+            <IconButton>
+              <InfoIcon />
+            </IconButton>
+          </Tooltip>
+        </Box>
+      </Stack>
+    ),
+  });
+
   return (
     <Box>
-      <Box>
-        <MaterialReactTable
-          columns={PreciosColumns}
-          data={PreciosData}
-          state={{ isLoading: loadingTable }}
-          initialState={{ density: "compact", showGlobalFilter: true }}
-          renderTopToolbarCustomActions={({ table }) => (
-            <>
-              {/* ------- BARRA DE ACCIONES ------ */}
-              <Stack direction="row" sx={{ m: 1 }}>
-                <Box>
-                  <Tooltip title="Agregar">
-                    <IconButton onClick={() => setAddPrecioShowModal(true)}>
-                      <AddCircleIcon />
-                    </IconButton>
-                  </Tooltip>
-                  <Tooltip title="Editar">
-                    <IconButton>
-                      <EditIcon />
-                    </IconButton>
-                  </Tooltip>
-                  <Tooltip title="Eliminar">
-                    <IconButton>
-                      <DeleteIcon />
-                    </IconButton>
-                  </Tooltip>
-                  <Tooltip title="Detalles ">
-                    <IconButton>
-                      <InfoIcon />
-                    </IconButton>
-                  </Tooltip>
-                </Box>
-              </Stack>
-              {/* ------- BARRA DE ACCIONES FIN ------ */}
-            </>
-          )}
-        />
-      </Box>
-      {/* M O D A L E S */}
+      <MaterialReactTable table={table} />
       <Dialog open={AddPrecioShowModal}>
         <AddPrecioModal
           AddPrecioShowModal={AddPrecioShowModal}
@@ -125,7 +125,5 @@ const PreciosTable = () => {
     </Box>
   );
 };
+
 export default PreciosTable;
-
-
-
--- src/security/promociones/components/tables/PromocionesTable.jsx
+++ src/security/promociones/components/tables/PromocionesTable.jsx
@@ -1,113 +1,131 @@
-//FIC: React
 import React, { useEffect, useMemo, useState } from "react";
-//FIC: Material UI
-import { MaterialReactTable } from "material-react-table";
-import { Box, Stack, Tooltip, Button, IconButton, Dialog } from "@mui/material";
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+} from "material-react-table";
+import { Box, Stack, Tooltip, IconButton, Dialog } from "@mui/material";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import EditIcon from "@mui/icons-material/Edit";
 import InfoIcon from "@mui/icons-material/Info";
 import DeleteIcon from "@mui/icons-material/Delete";
-//FIC: DB
-//import PromocionesStaticData from '../../../../../db/security/json/Promociones/PromocionesData';
 import { getAllPromociones } from "../../services/remote/get/getAllPromociones";
-//FIC: Modals
 import AddPromocionesModal from "../modals/AddPromocionesModal";
-//FIC: Columns Table Definition.
-const PromocionesColumns = [
 
-  {
-    accessorKey: "DesPromo",
-    header: "DESCUENTO DE PROMOCION",
-    size: 150, //small column
-  },
-  {
-    accessorKey: "IdTipoPromoOK",
-    header: "ID TIPO PROMOCION OK",
-    size: 150, //small column
-  },
-  {
-    accessorKey: "Formula",
-    header: "FORMULA",
-    size: 150, //small column
-  },
-];
-//FIC: Table - FrontEnd.
 const PromocionesTable = () => {
-  //FIC: controlar el estado del indicador (loading).
   const [loadingTable, setLoadingTable] = useState(true);
-
-  //FIC: controlar el estado de la data de Institutos.
   const [PromocionesData, setPromocionesData] = useState([]);
-  //FIC: controlar el estado que muesta u oculta la modal de nuevo Instituto.
-  const [AddPromocioneshowModal, setAddPromocioneshowModal] = useState(false);
+  const [rowSelection, setRowSelection] = useState({});
+  const [AddPromocionesShowModal, setAddPromocionesShowModal] = useState(false);
+  const [selectedPromocionId, setSelectedPromocionId] = useState(null);
+
   useEffect(() => {
     async function fetchData() {
       try {
         const AllPromocionesData = await getAllPromociones();
         setPromocionesData(AllPromocionesData);
-        //setPromocionesData(PromocionesStaticData);
         setLoadingTable(false);
       } catch (error) {
         console.error(
-          "Error al obtener los institutos en useEffect de PromocionesTable:",
+          "Error al obtener las promociones en useEffect de PromocionesTable:",
           error
         );
       }
     }
     fetchData();
   }, []);
+
+  const handleRowClick = (row) => {
+    setSelectedPromocionId(row.original._id);
+    setRowSelection((prev) => ({
+      [row.id]: !prev[row.id],
+    }));
+  };
+
+  const PromocionesColumns = useMemo(
+    () => [
+      { accessorKey: "DesPromo", header: "DESCUENTO DE PROMOCION", size: 150 },
+      {
+        accessorKey: "IdTipoPromoOK",
+        header: "ID TIPO PROMOCION OK",
+        size: 150,
+      },
+      { accessorKey: "Formula", header: "FORMULA", size: 150 },
+    ],
+    []
+  );
+
+  const table = useMaterialReactTable({
+    columns: PromocionesColumns,
+    data: PromocionesData,
+    getRowId: (row) => row._id,
+    muiTableBodyRowProps: ({ row }) => ({
+      onClick: () => handleRowClick(row),
+      selected: !!rowSelection[row.id],
+      sx: {
+        cursor: "pointer",
+        backgroundColor: rowSelection[row.id] ? "lightgreen" : "white",
+      },
+    }),
+    onRowSelectionChange: setRowSelection,
+    state: { rowSelection, isLoading: loadingTable },
+    renderBottomToolbarCustomActions: () => (
+      <Stack direction="row" sx={{ m: 1 }}>
+        <Box>
+          <Tooltip title="Agregar">
+            <IconButton onClick={() => setAddPromocionesShowModal(true)}>
+              <AddCircleIcon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Editar">
+            <IconButton
+              onClick={() => {
+                if (selectedPromocionId !== null) {
+                  // Implement edit logic here
+                } else {
+                  alert(
+                    "Por favor, seleccione una promoción antes de editarla"
+                  );
+                }
+              }}
+            >
+              <EditIcon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Eliminar">
+            <IconButton>
+              <DeleteIcon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Detalles">
+            <IconButton
+              onClick={() => {
+                if (selectedPromocionId !== null) {
+                  // Implement details logic here
+                } else {
+                  alert("Por favor, selecciona una fila para ver detalles.");
+                }
+              }}
+            >
+              <InfoIcon />
+            </IconButton>
+          </Tooltip>
+        </Box>
+      </Stack>
+    ),
+  });
+
   return (
     <Box>
-      <Box>
-        <MaterialReactTable
-          columns={PromocionesColumns}
-          data={PromocionesData}
-          state={{ isLoading: loadingTable }}
-          initialState={{ density: "compact", showGlobalFilter: true }}
-          renderTopToolbarCustomActions={({ table }) => (
-            <>
-              {/* ------- BARRA DE ACCIONES ------ */}
-              <Stack direction="row" sx={{ m: 1 }}>
-                <Box>
-                  <Tooltip title="Agregar">
-                    <IconButton onClick={() => setAddPromocioneshowModal(true)}>
-                      <AddCircleIcon />
-                    </IconButton>
-                  </Tooltip>
-                  <Tooltip title="Editar">
-                    <IconButton>
-                      <EditIcon />
-                    </IconButton>
-                  </Tooltip>
-                  <Tooltip title="Eliminar">
-                    <IconButton>
-                      <DeleteIcon />
-                    </IconButton>
-                  </Tooltip>
-                  <Tooltip title="Detalles ">
-                    <IconButton>
-                      <InfoIcon />
-                    </IconButton>
-                  </Tooltip>
-                </Box>
-              </Stack>
-              {/* ------- BARRA DE ACCIONES FIN ------ */}
-            </>
-          )}
-        />
-      </Box>
-      {/* M O D A L E S */}
-      <Dialog open={AddPromocioneshowModal}>
+      <MaterialReactTable table={table} />
+      <Dialog open={AddPromocionesShowModal}>
         <AddPromocionesModal
-          AddPromocioneshowModal={AddPromocioneshowModal}
-          setAddPromocioneshowModal={setAddPromocioneshowModal}
-          onClose={() => setAddPromocioneshowModal(false)}
+          AddPromocionesShowModal={AddPromocionesShowModal}
+          setAddPromocionesShowModal={setAddPromocionesShowModal}
+          onClose={() => setAddPromocionesShowModal(false)}
         />
       </Dialog>
     </Box>
   );
 };
+
 export default PromocionesTable;
-
-
-
